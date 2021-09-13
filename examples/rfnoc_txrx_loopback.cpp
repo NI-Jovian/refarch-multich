e02763464495346b86bf61c19ab685b194912623
Dylan Baros <dylan.baros@ni.com>
2021-09-13 11:33:49 -0500

    Removed old code

--- examples/rfnoc_txrx_loopback.cpp
+++ examples/rfnoc_txrx_loopback.cpp
@@ -143,13 +143,11 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
     // Allow for some setup time
     //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     //Build Streams
-    GraphAssembly::buildStreamsMultithread16(graphStruct, device, signal);
+    GraphAssembly::buildStreamsMultithread(graphStruct, device, signal);
     //Connect Graph
-    GraphAssembly::connectGraphMultithread16(graphStruct, signal);
     //Commit Graph
     GraphAssembly::commitGraph(graphStruct);
      // Allow for some setup time
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     //Load Replay Block Buffers with data to transmit
     ReplayControl::importData(graphStruct, signal);
     //Sync time across devices
